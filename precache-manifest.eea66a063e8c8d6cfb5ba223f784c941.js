9affbc5997a640d4de579042f24ae49e68a53800
matdem <math.demers@protonmail.com>
2020-10-01 15:42:32 -0400

    Updates

---
+++ precache-manifest.eea66a063e8c8d6cfb5ba223f784c941.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387bcb7000685076f0fc4548dc843e44",
+    "url": "/ZTM_robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/ZTM_robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "68c081b373d1756f8eb2",
+    "url": "/ZTM_robofriends/static/css/main.6317fc8c.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/ZTM_robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ZTM_robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c081b373d1756f8eb2",
+    "url": "/ZTM_robofriends/static/js/main.a20001ae.chunk.js"
+  },
+  {
+    "revision": "254ca1b858723c31b34a",
+    "url": "/ZTM_robofriends/static/js/runtime-main.9bab4df4.js"
+  },
+  {
+    "revision": "d8dfe011619a37429bcc00fdc96e44a9",
+    "url": "/ZTM_robofriends/static/media/CronicaGradient.d8dfe011.woff"
+  }
+]);
